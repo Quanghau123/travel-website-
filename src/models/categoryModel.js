87daf25d8840c5fb4e302b069856106664a50e2c
Quang Hau <l3acasha@gmail.com>
2025-04-11 16:25:56 +0700

    update model category

--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const categorySchema = new mongoose.Schema({
     CategoryName: { type: String, required: true },
     Description: { type: String },
+    Image: { type: String },
 }, {
     versionKey: false,
     timestamps: true
